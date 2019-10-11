0b762e0991d2a139081fa2911f9bea197f56e1a2
Jeff148 <54152340+Jeff148@users.noreply.github.com>
2019-10-11 09:53:16 -0300

    Create (Time Trial)DivisãoImpério.py

---
+++ 11-10-19/(Time Trial)DivisãoImpério.py
@@ -0,0 +1,11 @@
+N = int(input("Diga quantas cidades originalmente o ímperio possui: "))
+# N <= 10**5
+# 2 <= N <= 10**5
+# 1 <= A,B <= N
+
+cont = 0
+while cont != -1:
+	print("digite a distancia das cidades")
+	cidadeA = int(input())
+	cidadeB = int(input())
+	cont += 1
